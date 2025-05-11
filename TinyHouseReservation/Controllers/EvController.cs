d971312ea091accb1ac1103d1dbbe8b907dbd461
Mustafa Dinç <30298206+mustafadincmd@users.noreply.github.com>
2025-05-12 00:22:14 +0300

    Update EvController.cs

--- TinyHouseReservation/Controllers/EvController.cs
+++ TinyHouseReservation/Controllers/EvController.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using TinyHouseReservations.DataAccess;
+using TinyHouseReservations.Models;
 
 namespace TinyHouseReservations.Controllers
 {
     public class EvController : Controller
     {
+        private readonly EvRepository _evRepo = new EvRepository();
+
         public IActionResult Index()
         {
-            return View();
+//burada evleri listeliyorsanız giriş yaptığınız ev sahibinin Id si ile evleriGetir metoduna gönderin
+
+            var evler = _evRepo.EvleriGetir(2);
+            return View(evler);
+        }
+        // buraya ev ekleme metodu yazın ama önce ev ekleme sayfasını oluşturun
+        // ev ekleme metodunda hangi kullanıcıyı sessionda tutuyorsanız
+        // yani giriş yapan kullanıcının Id sini bulup o Kullanıcı Id ile Ev oluştur metodunu yazın.
+
+        public IActionResult EvEkle(Ev ev)
+        {
+//buradaki datayı ön yüzde oluşturduğunuz formdan almanız lazım.
+            ev.Baslik = "Ev Başlığı";
+            ev.Aciklama = "Ev Açıklaması";
+            ev.Konum = "Ev Konumu";
+            ev.Fiyat = 1000;
+            ev.Durum = true;
+            ev.GorselYolu = "ev.jpg";
+            ev.EvSahibiID = 2; // Giriş yapan kullanıcının ID'si
+            // Ev ekleme işlemi
+            _evRepo.EvEkle(ev);
+
+            return RedirectToAction("Index");
         }
     }
 }
